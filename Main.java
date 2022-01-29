18586b19969d6f85d5d7ee649ca4d20d7f2a8729
Shell2016 <bvg2003@bk.ru>
2022-01-29 23:01:56 +0300

    Stage 5 completed

--- Main.java
+++ Main.java
@@ -1,25 +1,37 @@
 package encryptdecrypt;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
-    static String mode = "enc";
-    static int key = 0;
-    static String data = "";
+    public static String mode = "enc";
+    public static int key = 0;
+    public static String data = "";
+    public static String outputFile = null;
+
 
     public static void main(String[] args) {
-        List<String> argsList = List.of(args);
-        setParams(argsList);
+        setParams(List.of(args));
 
-        switch (mode) {
-            case "enc":
-                System.out.println(encrypt(data, key));
-                break;
-            case "dec":
-                System.out.println(decrypt(data, key));
-                break;
-            default:
-                System.out.println("Wrong input!");
+        if (!"dec".equals(mode) && !"enc".equals(mode)) {
+            System.out.println("Wrong input!");
+        } else if ("dec".equals(mode)) {
+            key = -key;
+        }
+
+        if (outputFile == null) {
+            System.out.println(encrypt(data, key));
+        } else {
+            File file = new File(outputFile);
+            try (FileWriter writer = new FileWriter(file)) {
+                writer.write(encrypt(data, key));
+            } catch (IOException e) {
+                System.out.println("Error occurred! Some problems with output file");
+            }
         }
     }
 
@@ -41,9 +53,9 @@ public class Main {
         return sb.toString();
     }
 
-    public static String decrypt(String message, int key) {
-        return encrypt(message, -key);
-    }
+//    public static String decrypt(String message, int key) {
+//        return encrypt(message, -key);
+//    }
 
     public static void setParams(List<String> argsList) {
         if (argsList.contains("-mode")) {
@@ -68,6 +80,30 @@ public class Main {
                 data = argsList.get(argsList.indexOf("-data") + 1);
             } catch (ArrayIndexOutOfBoundsException e) {
                 System.out.println("Wrong data input");
+            }
+        } else if (argsList.contains("-in")) {
+            try {
+                String fileName = argsList.get(argsList.indexOf("-in") + 1);
+                File file = new File(fileName);
+                try (Scanner scanner = new Scanner(file)) {
+                    if (scanner.hasNext()) {
+                        data = scanner.nextLine();
+                        System.out.println("Data " + data);
+                    }
+                } catch (FileNotFoundException e) {
+                    System.out.println("Error occurred! Data file not found");
+                }
+
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        if (argsList.contains("-out")) {
+            try {
+                outputFile = argsList.get(argsList.indexOf("-out") + 1);
+            } catch (IndexOutOfBoundsException e) {
+                System.out.println("Error occurred! Please enter name for output file");
             }
         }
     }
